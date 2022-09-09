f7870c798fdb86036b6a5accaf266d4fedf0c806
infobiloa <109538180+infobiloa@users.noreply.github.com>
2022-09-08 20:00:25 -0500

    Add files via upload

---
+++ embed/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Embed Code Generator </title>
+</head>
+<body>
+    <iframe width="560" height="315" src="http://localhost/Google-Drive-Video-Player-main/embed.php?id=1hOs8T2pGPNx46mJdcB52P_XtrA0lpCce" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+</body>
+</html>
